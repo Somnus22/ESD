78f41ffe712791e2004788d2defeaec3e2537dcb
elliot <elliot.tan.2022@scis.smu.edu.sg>
2024-03-07 09:09:04 +0800

    in progress

--- Report.py
+++ Report.py
@@ -51,66 +51,18 @@ class Damages(db.Model):
 
 
 #Create a new report
-@app.route("/order")
-def get_all():
-    orderlist = db.session.scalars(db.select(Order)).all()
-    if len(orderlist):
-        return jsonify(
-            {
-                "code": 200,
-                "data": {
-                    "orders": [order.json() for order in orderlist]
-                }
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "message": "There are no orders."
-        }
-    ), 404
+@app.route("/report", methods=['POST'])
+def create_report():
 
-
-@app.route("/order/<string:order_id>")
-def find_by_order_id(order_id):
-    order = db.session.scalars(
-        db.select(Order).filter_by(order_id=order_id).limit(1)).first()
-    if order:
-        return jsonify(
-            {
-                "code": 200,
-                "data": order.json()
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "data": {
-                "order_id": order_id
-            },
-            "message": "Order not found."
-        }
-    ), 404
-
-
-@app.route("/order", methods=['POST'])
-def create_order():
-    customer_id = request.json.get('customer_id', None)
-    order = Order(customer_id=customer_id, status='NEW')
-
-    cart_item = request.json.get('cart_item')
-    for item in cart_item:
-        order.order_item.append(Order_Item(
-            book_id=item['book_id'], quantity=item['quantity']))
 
     try:
-        db.session.add(order)
+        db.session.add()
         db.session.commit()
     except Exception as e:
         return jsonify(
             {
                 "code": 500,
-                "message": "An error occurred while creating the order. " + str(e)
+                "message": "An error occurred while creating the report. " + str(e)
             }
         ), 500
 
@@ -120,46 +72,6 @@ def create_order():
             "data": order.json()
         }
     ), 201
-
-
-@app.route("/order/<string:order_id>", methods=['PUT'])
-def update_order(order_id):
-    try:
-        order = db.session.scalars(
-        db.select(Order).filter_by(order_id=order_id).
-        limit(1)).first()
-        if not order:
-            return jsonify(
-                {
-                    "code": 404,
-                    "data": {
-                        "order_id": order_id
-                    },
-                    "message": "Order not found."
-                }
-            ), 404
-
-        # update status
-        data = request.get_json()
-        if data['status']:
-            order.status = data['status']
-            db.session.commit()
-            return jsonify(
-                {
-                    "code": 200,
-                    "data": order.json()
-                }
-            ), 200
-    except Exception as e:
-        return jsonify(
-            {
-                "code": 500,
-                "data": {
-                    "order_id": order_id
-                },
-                "message": "An error occurred while updating the order. " + str(e)
-            }
-        ), 500
 
 
 if __name__ == '__main__':
