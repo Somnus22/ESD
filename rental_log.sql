e3afb1f431642c7b70df1ae92c5ec2afa57f8d06
WASDuser <william.tan.2022@scis.smu.edu.sg>
2024-03-17 13:01:30 +0800

    Update rental_log.sql

--- rental_log.sql
+++ rental_log.sql
@@ -1,9 +1,12 @@
-CREATE TABLE rental_log (
-    LogID INT AUTO_INCREMENT PRIMARY KEY,
-    LogEntryTime DATETIME NOT NULL,
-    LogEntryDesc VARCHAR(255) NOT NULL,
-    CarID INT,  -- Foreign key reference to Car table
-    UserID INT,  -- Foreign key reference to Users table
+SET time_zone = '+08:00';
+DROP TABLE IF EXISTS 'rental log';
+
+CREATE TABLE IF NOT EXISTS 'rental_log' (
+    'LogID' INT PRIMARY KEY,
+    'LogEntryTime' TIMESTAMP NOT NULL CURRENT_TIMESTAMP,
+    'LogEntryDesc' VARCHAR(255) NOT NULL,
+    'CarID' INT,  
+    'UserID' INT, 
     FOREIGN KEY (CarID) REFERENCES car(CarID),
     FOREIGN KEY (UserID) REFERENCES users(userID)
-);
+); 
