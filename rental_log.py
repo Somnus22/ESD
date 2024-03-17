c98ece9f4b7b73bcfc646b859c9e0e4d3516ee7c
WASDuser <william.tan.2022@scis.smu.edu.sg>
2024-03-17 13:33:46 +0800

    Update rental_log.py

--- rental_log.py
+++ rental_log.py
@@ -1,91 +1,51 @@
+#!/usr/bin/env python3
+# The above shebang (#!) operator tells Unix-like environments
+# to run this file as a python3 script
+
+import os
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:8892/esd' # change to 3306 on my pc
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class RentalLog(db.Model):
-    __tablename__ = 'rental_log'
-
-    logID = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    logEntryTime = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    logEntryDesc = db.Column(db.String(255), nullable=False)
-    
-    # TBC: supposed to reference Car Inventory and User Tables
-    
-    # carID = db.Column(db.Integer, db.ForeignKey('car.carID'))
-    # car = relationship("car", back_populates="rental_logs")
-    # # params: <child>,<parent>
-    # # TBC: backref? (https://stackoverflow.com/questions/51335298/concepts-of-backref-and-back-populate-in-sqlalchemy)
-    
-    # userID = db.Column(db.Integer, db.ForeignKey('user.userID'))
-    # user = relationship("user", back_populates="rental_logs")
-    
-    carID = db.Column(db.Integer, nullable=False)
-    userID = db.Column(db.Integer, nullable=False)
-
-    def json(self):
-        return {
-            "logID": self.logID,
-            "logEntryTime": str(self.logEntryTime),
-            "logEntryDesc": self.logEntryDesc,
-            "carID": self.carID,
-            "userID": self.userID,
-        }
+CORS(app)
 
 @app.route("/rental_log", methods=['POST'])
-def create_rental_log():
-    data = request.get_json();
-    
-    logID = data.get("logID")
-    log_entry_time = data.get("logEntryTime")
-    log_entry_desc = data.get("logEntryDesc")
-    carID = data.get("carID")
-    userID = data.get("userID")
+def createRentalLog():
+    # JSON data validation
+    rental_data = None
+    if request.is_json:
+        rental_data = request.get_json()
+        result = processRentalLog(rental_data)
+        return jsonify(result), result["code"]
+    else:
+        data = request.get_data()
+        print("Received an invalid rental log entry:")
+        print(data)
+        return jsonify({"code": 400,
+                        "data": str(data),
+                        "message": "Ensure that rental log entry is in JSON format."}), 400
+        
+        
+@app.route("/rental_log/<int:logID>", methods=['PUT'])
+def cancelLogEntry(logID):
+    data = request.get_json()
+    log_to_be_deleted = data.get("logID")
 
-    rental_log = RentalLog(logID=logID, logEntryTime=log_entry_time, logEntryDesc=log_entry_desc, carID=carID, userID=userID)
-
-    try:
-        db.session.add(rental_log)
-        db.session.commit()
-    except Exception as e: # catches all exceptions indiscriminately
-        return jsonify(
-            {
-                "code": 500,
-                "message": f"An error occurred adding the rental log: {str(e)}"
-            }
-        ), 500
-
-    return jsonify(
-        {
-            "code": 201,
-            "data": rental_log.json()
-        }
-    ), 201
-
-@app.route("/rental_logs/<int:logID>", methods=['PUT']) # PUT cuz updating resource
-def cancel_rental_log(logID):
-    rental_log = RentalLogs.query.get(logID)
-
-    if rental_log:
+    if log_to_be_deleted:
         try:
-            db.session.delete(rental_log)
+            db.session.delete(log_to_be_deleted)
             db.session.commit()
             return jsonify(
                 {
                     "code": 200,
-                    "message": f"Rental log with logID {logID} has been cancelled."
+                    "message": f"Rental log with LogID {logID} has been removed."
                 }
             ), 200
         except Exception as e:
             return jsonify(
                 {
                     "code": 500,
-                    "message": f"An error occurred cancelling the rental log: {str(e)}"
+                    "message": f"An error occurred removing the rental log: {str(e)}"
                 }
             ), 500
     else:
@@ -95,6 +55,31 @@ def cancel_rental_log(logID):
                 "message": "Rental log not found."
             }
         ), 404
+        
+        
+def processRentalLog(rental_data):
+    print("Processing a rental log entry:")
+    print(rental_data)
+    
+    if "ERROR" in rental_data['userID']:
+        code = 400
+        message = 'Failed creation of rental log entry.'
+    else:
+        code = 201
+        message = 'Successful creation of rental log entry.'
+    
+    print(message+'\n')
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    return {
+        'code': code,
+        'data': {
+            'logID': rental_data['logID']
+        },
+        'message': message
+    }
+
+# Execute this program only if it is run as a script (not by 'import')
+if __name__ == "__main__":
+    print("This is flask " + os.path.basename(__file__) + ": handling rental log entries ...")
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
